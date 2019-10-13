ef0d329f1e22275922657cc53085335a843ea09d
Edson Ayllon <eayllon1@gmail.com>
2019-10-13 19:50:42 -0400

    add linegraph

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import { Chart } from 'react-charts';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data = React.useMemo(
+    () => [
+      {
+        label: 'Series 1',
+        data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+      },
+      {
+        label: 'Series 2',
+        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+      }
+    ],
+    []
+  )
+ 
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' }
+    ],
+    []
+  )
+ 
+  const lineChart = (
+    // A react-chart hyper-responsively and continuusly fills the available
+    // space of its parent element automatically
+    <div
+      style={{
+        width: '400px',
+        height: '300px'
+      }}
+    >
+      <Chart data={data} axes={axes} />
+    </div>
+  )
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +54,11 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
+      <div style={{padding: 50}}>
+        {lineChart}
+      </div>
     </div>
   );
 }
